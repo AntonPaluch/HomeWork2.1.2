5c79c16715b8c9117b2857ef959c27104fdb5c42
Dmitry Pavlov <perr07@yandex.ru>
2021-01-27 22:02:15 +0500

    work code

--- HomeWork2.1.2/ViewController.swift
+++ HomeWork2.1.2/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var buttonStart: UIButton!
+    
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.alpha=0.3
+        yellowView.alpha=0.3
+        greenView.alpha=0.3
+        redView.layer.cornerRadius=60
+        yellowView.layer.cornerRadius=60
+        greenView.layer.cornerRadius=60
+        buttonStart.layer.cornerRadius=10
     }
 
 
-}
+ 
+    @IBAction func startButtonPressed(_ sender: UIButton)
+    {
+    buttonStart.setTitle("Next", for: .normal)
+        redView.alpha=1
+    //логику не смог реализовать 
+        }
+
+    }
+    
+
 
